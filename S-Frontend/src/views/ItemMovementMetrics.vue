981ca927530f583f64be4e311dd769f6fd8c63e3
Kian Jacob Laxamana <142416427+Theycallmeki@users.noreply.github.com>
2026-03-12 15:09:36 +0800

    Update ItemMovementMetrics.vue

--- S-Frontend/src/views/ItemMovementMetrics.vue
+++ S-Frontend/src/views/ItemMovementMetrics.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, onMounted } from "vue"
 import { useRouter } from "vue-router"
-import api from "../services/api"   // ✅ USE AXIOS INSTANCE
+import api from "../services/api"   
 
 const router = useRouter()
 
